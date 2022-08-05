fb751c8c7fc5b1b3756e85d483c7d123284f2810
nhnahid1122 <nhnahid1122@gmail.com>
2022-08-06 00:28:00 +0200

    first commit

---
+++ src/main/java/com/example/fletnixwithspring/FletnixWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.fletnixwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FletnixWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FletnixWithSpringApplication.class, args);
+    }
+
+}
