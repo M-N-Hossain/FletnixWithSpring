ae9d93583247d99cf66d096ddc92b7fcc4ee6108
nhnahid1122 <nhnahid1122@gmail.com>
2022-08-06 00:34:25 +0200

    Rename the class name

---
+++ src/main/java/com/example/fletnixwithspring/Controller/HomepageController.java
@@ -0,0 +1,5 @@
+package com.example.fletnixwithspring.Controller;
+
+public class HomepageController {
+
+}
--- src/main/java/com/example/fletnixwithspring/Controller/HompageController.java
+++
@@ -1,2 +0,0 @@
-package com.example.fletnixwithspring.Controller;public class HompageController {
-}
